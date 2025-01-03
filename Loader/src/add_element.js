62c404fb424e8daaa05fda22ee60c1d875e9cb04
mostazaniikkkk <ser.valle@alumnos.duoc.cl>
2025-01-03 15:06:05 -0300

    The parche de emergencia update

--- Loader/src/add_element.js
+++
@@ -1,7 +0,0 @@
-function addElement(element, id, func){
-    const htmlString = element;
-    const element = document.getElementById(id);
-    element.innerHTML += htmlString; 
-
-    element.addEventListener("click", func);
-}
---
+++ Loader/src/add_remote.js
@@ -0,0 +1,8 @@
+addElement(
+    "remote",
+    "remoto",
+    `<select name="remote" id="remote">
+        <option value="si">Si</option>
+        <option value="no">No</option>
+    </select>`
+);
